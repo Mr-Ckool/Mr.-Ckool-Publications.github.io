8ac3b85dd9ed82790a637ebf993a7a8d081fb711
Mr-Ckool <75772094+Mr-Ckool@users.noreply.github.com>
2022-06-04 20:49:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 window.onload = function () {
     var images = [
-        'image1.jpeg',
         'image2.jpeg',
         'image3.jpeg',
-        'image4.jpeg',
-        'image5.jpeg',
         'image6.jpeg',
-        'image7.jpeg',
-        'image8.jpeg',
-        'image9.jpeg',
         'image10.jpeg'
     ];
 
