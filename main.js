9ce1ef054addc0c6accda6d838d63d2c2c6c3652
Mr-Ckool <75772094+Mr-Ckool@users.noreply.github.com>
2022-06-06 13:01:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var images = [
-        'image1.jpeg',
+        'image3.jpeg',
     ];
 
     document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random()*images.length)] + ')';
